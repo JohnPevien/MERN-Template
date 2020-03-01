0af4a2c08e203349fb6c253f74fdc19d4aa14d1c
John Pevien Laguindam <johnpevienwork@gmail.com>
2020-03-01 17:07:17 +0800

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,17 @@
+
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./prod');
+}
+else{
+    module.exports = require('./dev');
+}
+
+/*
+Format:
+module.exports = {
+    googleClientID: "insert google client id",
+    googleClientSecret: "insert google client secret",
+    mongoURI:"insert mongo uri",
+    cookieKey: 'insert cookie'
+}
+*/
--- config/keysTemplate.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    googleClientID: "insert google auth client id",
-    googleClientSecret: "insert google auth client secret",
-    mongoURI:'insert mongoURI',
-    cookieKey: 'insert cookieKey'
-}
