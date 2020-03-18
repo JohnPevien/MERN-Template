beb67c76ac564993ee55fa4e1f938d14b8324776
John Pevien Laguindam <johnpevienwork@gmail.com>
2020-03-19 01:13:52 +0800

    Added billing functionality with stripe
    - Added billing functionality with stripe
    - moved prettier

--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
-const moongose = require('mongoose');
-const {Schema} = moongose;
-
+const moongose = require('mongoose')
+const { Schema } = moongose
 
 const userSchema = new Schema({
-    googleId: String
-});
+  googleId: String,
+  credits: { type: Number, default: 0 }
+})
 
-moongose.model('users', userSchema);
+moongose.model('users', userSchema)
