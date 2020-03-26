cc12c5c52df0ecce22c07ffb040df42e7d074ebd
John Pevien Laguindam <johnpevienwork@gmail.com>
2020-03-27 01:53:55 +0800

    Added mailing function

---
+++ services/emailTemplates/surveyTemplate.js
@@ -0,0 +1,18 @@
+const keys = require('../../config/keys')
+
+module.exports = survey => {
+  return `
+    <html>
+      <body>
+      <div style="text-align:center;">
+        <h3>I'd like your input!</h3>
+        <p> ${survey.subject}
+        </p>
+        <p> ${survey.body} </p>
+        <div><a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a></div>
+        <div><a href="${keys.redirectDomain}/api/surveys/thanks">No</a></div>
+      </div>
+      </body>
+    </html>
+  `
+}
